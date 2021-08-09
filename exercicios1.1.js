9c28535351ce6c78bf8ee815f3ace3bff5cdce98
davi <davisoarescoura@gmail.com>
2021-08-10 00:33:00 +0200

    exercicios 3 concluido

--- exercicios1.1.js
+++ exercicios1.1.js
@@ -1,4 +1,6 @@
 document.getElementById("paragrafo").innerText = "Daqui a dois anos eu pretendo estar ja estabilizado em uma boa empresa , com uma estabilidade, morando no Brasil novamente e com uma realizacao profissional ";
-document.getElementById("paragrafo").style.color = "yellow"
-document.getElementsByClassName("main-content")[0].style.backgroundColor = "red"
+document.getElementById("paragrafo").style.color = "green"
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "green";
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "white"
+
 
